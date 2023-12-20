b43587927f0c0ed5baeb22c8f44b722485aef9b7
Colton Provias <cj@coltonprovias.com>
2023-12-20 16:58:05 -0500

    Add Angular Material.

--- apps/evaluator/src/app/app.config.ts
+++ apps/evaluator/src/app/app.config.ts
@@ -3,10 +3,12 @@ import { provideRouter } from '@angular/router';
 import {APP_BASE_HREF} from '@angular/common';
 
 import { routes } from './app.routes';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    {provide: APP_BASE_HREF, useValue: '/evaluator'},
-  ]
+    { provide: APP_BASE_HREF, useValue: '/evaluator' },
+    provideAnimations()
+]
 };
